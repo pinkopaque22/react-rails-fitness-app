740eb500e2c17b6758d91f33345dda3c1f940203
Patricia Ehrhardt <pinkopaque22@gmail.com>
2025-04-16 15:54:49 -0500

    Fix migration issue by running the migration. Add the actions to the create method in the controller.

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -1,13 +1,27 @@
 class Api::V1::ExercisesController < ApplicationController
   def index
+    exercise = Exercise.all.order(created_at: :desc)
+    render json: exercise
   end
 
   def create
+    exercise = Exercise.create!(exercise_params)
+    if exercise
+      render json: exercise
+    else
+      render json: exercise_errors
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def exercise_params
+    params.permit(:name, :image, :trainings, :instruction)
   end
 end
