414eeb061626c433ad9d67613e9a16a5d56d5de9
Patricia Ehrhardt <pinkopaque22@gmail.com>
2025-04-16 15:45:00 -0500

    Generate the exercises controller and routes, with CRUD actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get "exercises/index"
+      post "exercises/create"
+      get "/show/:id", to: "exercises#show"
+      delete "/destroy/:id", to: "exercises#destroy"
+    end
+  end
   root "homepage#index"
+  get "/*path" => "homepage#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
